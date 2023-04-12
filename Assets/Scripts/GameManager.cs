5c488a4b8ecca84916ca384de4f75e9e66cbfe6b
Ashley Roesler <ashley.n.roesler@gmail.com>
2023-04-11 19:32:33 -0500

    play button works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public string MainMenuSceneName;
+    public string GameSceneName;
+    public string EndSceneName;
+
+    public void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayGame(int pageIndex = 0) {
+
+        // to do: make player able to load to specific scene... how??? Page manager? page event?
+
+        SceneManager.LoadScene(GameSceneName);
+    }
+
     public void QuitGame() {
 
         // to do: make sure to save which pages the player has seen
